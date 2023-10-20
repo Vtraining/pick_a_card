a14023e883854a4fbd55ee86310ef0585d34df86
Vtraining <40.s.a.m.y.40@gmail.com>
2023-10-21 00:30:04 +0300

    Alternative output & Joker

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -8,8 +8,12 @@ cards = []
 
 values.each do |value|
   suits.each do |suit|
-    cards << "#{value} of #{suit}"
+    cards << "[#{value} of #{suit}]" 
   end
+end
+
+2.times do 
+  cards << '[Joker]'
 end
 
 cards.shuffle!
